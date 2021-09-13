0015e1ae04813fa5b7396100f2be76bf8a79f152
Yahavk94 <yahavk94@gmail.com>
2021-09-13 17:59:30 +0300

    Update

--- Node.h
+++ Node.h
@@ -8,9 +8,13 @@
 #define NODE_H
 
 struct Node {
-    enum Operator operator;
+    // the parent node of this node
     struct Node *parent;
 
+    // the operator that generated this node
+    enum Operator operator;
+
+    // the state of this node
     char state[CUBE_SIZE];
 };
 
